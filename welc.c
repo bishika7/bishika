58989c4caf7c7f155da7ad92f1c367257fa282e8
bishika7 <43375248+bishika7@users.noreply.github.com>
2018-09-18 17:58:43 +0530

    Create welc.c

---
+++ welc.c
@@ -0,0 +1,9 @@
+/* Fig. 2.1: fig02_01.c
+2 A first program in C */
+#include <stdio.h>
+/* function main begins program execution */
+int main( void )
+{
+printf( "Welcome to C!\n" );
+return 0; /* indicate that program ended successfully */
+} /* end function main */
