80b616820b823a7055e40a370477fa39e89bb5a5
bishika7 <43375248+bishika7@users.noreply.github.com>
2018-09-19 18:39:57 +0530

    Create ques.c

---
+++ ques.c
@@ -0,0 +1,21 @@
+1)what is compiling?
+2)what is int main and void?
+3)what is coding?
+4)what is the use of punctuations in programming?
+5)why only punctuations are used?
+6)why not any other language?
+7)why are brackets used?
+8)what is the utility of getting an output after compiling and running?
+9)what is the use of grey texts?
+10)what is the use of green texts?
+11)why are different colours used?
+12)what is github?why is it used?
+13)what is programming?
+14)what is c programming?
+15)why are the different types of languages used in programming?
+16)why is it categorised in levels?
+17)how to start writing a program?
+18)how to understand what is written in a program?
+19)what are codes?
+20)what happens after we learn each language/
+21)what can we do after learning each of it?
